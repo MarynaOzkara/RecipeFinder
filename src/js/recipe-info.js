ade834121d3c84eaec4946d9b30173511529cf2b
MarynaOzkara <ozkaramaryna@gmail.com>
2023-10-27 20:35:34 +0300

    fix rating list

--- src/js/recipe-info.js
+++ src/js/recipe-info.js
@@ -27,8 +27,8 @@ export function handleRecipeInfo(recipeId) {
       if (recipe.tags.length > 1) {
         tagsList.innerHTML = createTagsMarkup(recipe.tags);
       }
-      const ratingList = document.querySelector('.js-rating-items');
-      ratingList.innerHTML = handleStars();
+
+      handleStars();
       const ratedValue = Math.round(recipe.rating);
       const stars = document.querySelectorAll('.star-item');
       // console.log(stars);
